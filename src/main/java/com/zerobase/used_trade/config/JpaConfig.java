c1023141a0ddf8ca10f84d9888ca74628c0a91c3
amj9843 <owo9@kakao.com>
2024-06-27 02:35:11 +0900

    feat: 엔티티마다 들어갈 기본 엔티티 생성(생성 시간, 수정 시간)

---
+++ src/main/java/com/zerobase/used_trade/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.zerobase.used_trade.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
