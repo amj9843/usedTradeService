2c266faf310a19989d4da67eda5b1f6bcd2bd0ab
amj9843 <owo9@kakao.com>
2024-06-30 22:59:05 +0900

    feat: 상품 관련 Entity, Enum 생성

---
+++ src/main/java/com/zerobase/used_trade/data/constant/ProductStatus.java
@@ -0,0 +1,21 @@
+package com.zerobase.used_trade.data.constant;
+
+public enum ProductStatus {
+  CONSIGNMENT_APPLY("위탁 신청 상태"),
+  CONSIGNMENT_APPROVED("위탁 승인 상태"),
+  SELLING("판매중인 상태"),
+  PROCESSING("거래가 진행중인 상태"),
+  SELLED("판매 완료 상태"),
+  CANCELED_APPLY("위탁 취소 신청 상태"),
+  CANCELED("판매 취소 상태");
+
+  private final String description;
+
+  ProductStatus(String description) {
+    this.description = description;
+  }
+
+  public String description() {
+    return description;
+  }
+}
---
+++ src/main/java/com/zerobase/used_trade/data/domain/Product.java
@@ -0,0 +1,66 @@
+package com.zerobase.used_trade.data.domain;
+
+import com.zerobase.used_trade.data.constant.ProductStatus;
+import com.zerobase.used_trade.data.constant.Quality;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "product")
+@Entity
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "product_id")
+  private Long id;
+
+  @Column(name = "seller_id")
+  private Long sellerId;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "quality")
+  @Enumerated(EnumType.STRING)
+  private Quality quality;
+
+  @Column(name = "price")
+  private Long price;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "status")
+  @Enumerated(EnumType.STRING)
+  private ProductStatus status;
+
+  @Column(name = "consignment")
+  private boolean consignment;
+
+  public void updateStatus(ProductStatus status) {
+    this.status = status;
+  }
+
+  @Builder
+  public Product(
+      Long sellerId, String name, Quality quality, Long price, String description,
+      ProductStatus status, boolean consignment) {
+    this.sellerId = sellerId;
+    this.name = name;
+    this.quality = quality;
+    this.price = price;
+    this.description = description;
+    this.status = status;
+    this.consignment = consignment;
+  }
+}
