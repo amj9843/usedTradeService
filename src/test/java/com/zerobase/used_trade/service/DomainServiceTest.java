91fe2cd1eabd85632483b472c0ed97ac68c27f2d
amj9843 <owo9@kakao.com>
2024-07-07 16:05:19 +0900

    feat: Domain 관련 컨트롤러, 서비스, dto 유효성 테스트

---
+++ src/test/java/com/zerobase/used_trade/service/DomainServiceTest.java
@@ -0,0 +1,223 @@
+package com.zerobase.used_trade.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.zerobase.used_trade.data.constant.DomainFilterType;
+import com.zerobase.used_trade.data.constant.DomainSortType;
+import com.zerobase.used_trade.data.constant.EmployeeSortType;
+import com.zerobase.used_trade.data.constant.SubscribeType;
+import com.zerobase.used_trade.data.domain.Domain;
+import com.zerobase.used_trade.data.dto.DomainDto.DetailInfoResponse;
+import com.zerobase.used_trade.data.dto.DomainDto.EnrollRequest;
+import com.zerobase.used_trade.data.dto.DomainDto.Principle;
+import com.zerobase.used_trade.data.dto.DomainDto.UpdateRequest;
+import com.zerobase.used_trade.repository.DomainRepository;
+import com.zerobase.used_trade.service.impl.DomainServiceImpl;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+
+@ExtendWith(MockitoExtension.class)
+public class DomainServiceTest {
+  @InjectMocks
+  private DomainServiceImpl domainService;
+  @Mock
+  private DomainRepository domainRepository;
+
+  public static String domainAddress = "test.com";
+  public static String companyName = "test_company";
+  public static String businessNumber = "000-00-00000";
+  public static String zipCode = "00000";
+  public static String roadAddress = "fake_road_address";
+  public static String address = "fake_address";
+  public static String detail = "";
+  public static String phoneNumber = "000-0000-0000";
+  public static String period = "ONEYEAR";
+  public static String criteria = "NAMEASC";
+  public static String filter = "ALL";
+  public static int page = 0;
+  public static int size = 10;
+
+  @DisplayName("도메인 등록")
+  @Test
+  void enrollDomain() {
+    //given
+    EnrollRequest dto = new EnrollRequest(
+        domainAddress, companyName, businessNumber, zipCode, roadAddress,
+        address, detail, phoneNumber, period
+    );
+
+    given(domainRepository.existsByDomainAddress(anyString())).willReturn(false);
+    given(domainRepository.save(any())).willReturn(dto.toEntity());
+
+    //when
+    Principle domain = domainService.enrollDomain(dto);
+    ArgumentCaptor<Domain> domainCaptor = ArgumentCaptor.forClass(Domain.class);
+
+    //then
+    verify(domainRepository, times(1)).save(domainCaptor.capture());
+
+    assertThat(domainCaptor.getValue().getDomainAddress()).isEqualTo(domainAddress);
+    assertThat(domainCaptor.getValue().getCompanyName()).isEqualTo(companyName);
+    assertThat(domain.getDomainAddress()).isEqualTo(domainAddress);
+    assertThat(domain.getCompanyName()).isEqualTo(companyName);
+  }
+
+  @DisplayName("등록된 도메인 목록 조회")
+  @Test
+  void getDomainList() {
+    //given
+    EnrollRequest dto1 = new EnrollRequest(
+        domainAddress, companyName, businessNumber, zipCode, roadAddress,
+        address, detail, phoneNumber, period
+    );
+    EnrollRequest dto2 = new EnrollRequest(
+        "test2.com", "company_name2", businessNumber, zipCode, roadAddress,
+        address, detail, phoneNumber, period
+    );
+
+    given(domainRepository.findAll()).willReturn(Arrays.asList(
+        dto1.toEntity(), dto2.toEntity()
+    ));
+
+    //when
+    Page<Principle> domainPage = domainService.getDomainList(
+        null, null, 0, 10,
+        DomainSortType.valueOf(criteria), DomainFilterType.valueOf(filter));
+
+    //then
+    verify(domainRepository, times(1)).findAll();
+
+    assertThat(domainPage.getContent().size()).isEqualTo(2);
+    assertThat(domainPage.getContent().get(0).getDomainAddress()).isEqualTo("test2.com");
+    assertThat(domainPage.getContent().get(1).getDomainAddress()).isEqualTo(domainAddress);
+  }
+
+  @DisplayName("도메인 상세정보 조회")
+  @Test
+  void getDomainDetail() {
+    //given
+    Long domainId = 1L;
+
+    EnrollRequest dto = new EnrollRequest(
+        domainAddress, companyName, businessNumber, zipCode, roadAddress,
+        address, detail, phoneNumber, period
+    );
+
+    given(domainRepository.findById(anyLong())).willReturn(Optional.ofNullable(dto.toEntity()));
+
+    //when
+    DetailInfoResponse domainDetail = domainService.getDomainDetail(
+        1L, 0, 10, EmployeeSortType.valueOf(criteria));
+
+    //then
+    verify(domainRepository, times(1)).findById(domainId);
+
+    assertThat(domainDetail.getDomainAddress()).isEqualTo(domainAddress);
+    assertThat(domainDetail.getEmployees()).isEqualTo(null);
+  }
+
+  @DisplayName("도메인 기간 갱신")
+  @Test
+  void extendPeriodOfDomain() {
+    //given
+    Long domainId = 1L;
+
+    EnrollRequest dto = new EnrollRequest(
+        domainAddress, companyName, businessNumber, zipCode, roadAddress,
+        address, detail, phoneNumber, period
+    );
+
+    Domain domain = dto.toEntity();
+    LocalDateTime nowEndTime = domain.getEndAt();
+
+    given(domainRepository.findById(anyLong())).willReturn(Optional.of(domain));
+
+    //when
+    domainService.extendPeriodOfDomain(domainId, SubscribeType.valueOf(period));
+
+    //then
+    verify(domainRepository, times(1)).findById(domainId);
+
+    assertThat(domain.getEndAt()).isEqualTo(nowEndTime.plusMonths(12L));
+  }
+
+  @DisplayName("도메인 수정")
+  @Test
+  void updateDomainInfo() {
+    //given
+    Long domainId = 1L;
+
+    EnrollRequest enrollDto = new EnrollRequest(
+        domainAddress, companyName, businessNumber, zipCode, roadAddress,
+        address, detail, phoneNumber, period
+    );
+    UpdateRequest updateDto = new UpdateRequest(
+        "test2.com",
+        "change_name",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    );
+
+    Domain domain = enrollDto.toEntity();
+
+    given(domainRepository.findById(anyLong())).willReturn(Optional.of(domain));
+
+    //when
+    domainService.updateDomainInfo(domainId, updateDto);
+
+    //then
+    verify(domainRepository, times(1)).findById(domainId);
+
+    assertThat(domain.getDomainAddress()).isEqualTo("test2.com");
+    assertThat(domain.getCompanyName()).isEqualTo("change_name");
+    assertThat(domain.getBusinessNumber()).isEqualTo(businessNumber);
+  }
+
+  @DisplayName("도메인 삭제")
+  @Test
+  void deleteDomain() {
+    //given
+    Long domainId = 1L;
+
+    Domain domain = new Domain(
+        domainAddress,
+        companyName,
+        businessNumber,
+        zipCode,
+        roadAddress,
+        address,
+        detail,
+        phoneNumber,
+        LocalDateTime.now().minusMonths(1L)
+    );
+
+    given(domainRepository.findById(anyLong())).willReturn(Optional.of(domain));
+    willDoNothing().given(domainRepository).delete(any());
+
+    //when
+    domainService.deleteDomain(domainId);
+
+    //then
+    verify(domainRepository, times(1)).delete(any());
+  }
+}
---
+++ src/test/java/com/zerobase/used_trade/validator/DomainDtoValidationTest.java
@@ -0,0 +1,169 @@
+package com.zerobase.used_trade.validator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.zerobase.used_trade.data.dto.DomainDto.EnrollRequest;
+import com.zerobase.used_trade.data.dto.DomainDto.ExtensionRequest;
+import com.zerobase.used_trade.data.dto.DomainDto.UpdateRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DomainDtoValidationTest {
+  private static Validator validator;
+
+  @BeforeEach
+  void setUpValidator() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @DisplayName("DomainDto.EnrollRequest 유효성 검사")
+  @Test
+  void EnrollRequestValidation() {
+    //given
+    EnrollRequest dto =
+        new EnrollRequest(
+            "domain",
+            "  ",
+            "1111-11-11111",
+            "123456",
+            "  ",
+            "  ",
+            "   ",
+            "0123456789",
+            "잘못된값");
+
+    //when
+    Set<ConstraintViolation<EnrollRequest>> violations = validator.validate(dto);
+
+    //then
+    for (ConstraintViolation<EnrollRequest> violation: violations) {
+      switch (violation.getPropertyPath().toString()) {
+        case "domain":
+          if (dto.getDomainAddress().isEmpty()) {
+            assertThat(violation.getMessage()).isEqualTo("도메인 입력은 필수로, 빈 칸으로 입력할 수 없습니다.");
+          } else {
+            assertThat(violation.getMessage()).isEqualTo("도메인 형식이 잘못되었습니다.");
+          }
+          break;
+        case "companyName":
+          assertThat(violation.getMessage()).isEqualTo("회사명은 필수로, 빈 칸으로만 이루어질 수 없습니다.");
+          break;
+        case "businessNumber":
+          if (dto.getBusinessNumber().isEmpty()) {
+            assertThat(violation.getMessage()).isEqualTo("사업자 번호 입력은 필수로, 빈 칸으로 입력할 수 없습니다.");
+          } else {
+            assertThat(violation.getMessage()).isEqualTo("사업자 번호 형식이 잘못되었습니다. 사업자 번호는 000-00-00000 형식으로 입력되어야 합니다.");
+          }
+          break;
+        case "zipCode":
+          if (dto.getZipCode().isEmpty()) {
+            assertThat(violation.getMessage()).isEqualTo("우편번호 입력은 필수로, 빈 칸으로 입력할 수 없습니다.");
+          } else {
+            assertThat(violation.getMessage()).isEqualTo("우편번호 입력 형식이 잘못되었습니다. 우편번호는 숫자 5자리로만 입력되어야 합니다.");
+          }
+          break;
+        case "roadAddress":
+          assertThat(violation.getMessage()).isEqualTo("도로명 주소 입력은 필수로, 빈 칸으로만 이루어질 수 없습니다.");
+          break;
+        case "address":
+          assertThat(violation.getMessage()).isEqualTo("지번 입력은 필수로, 빈 칸으로만 이루어질 수 없습니다.");
+          break;
+        case "detail":
+          assertThat(violation.getMessage()).isEqualTo("미입력(null), 공백은 입력 가능하나 빈 칸으로만 이루어진 값은 입력할 수 없습니다.");
+          break;
+        case "phoneNumber":
+          if (dto.getPhoneNumber().isEmpty()) {
+            assertThat(violation.getMessage()).isEqualTo("회사 전화번호 입력은 필수로, 빈 칸으로 입력할 수 없습니다.");
+          } else {
+            assertThat(violation.getMessage()).isEqualTo("전화번호 입력 형식이 잘못되었습니다. 전화번호는 000-0000-0000 형식으로 입력되어야 합니다.");
+          }
+          break;
+        case "period":
+          assertThat(violation.getMessage()).contains("지정된 항목 중 하나를 입력해야 합니다. 입력 가능 항목(설명): ");
+        default:
+          break;
+      }
+    }
+    assertThat(violations.size()).isEqualTo(9); //틀리게 입력한 DTO 내 항목 개수
+  }
+
+  @DisplayName("DomainDto.ExtensionRequest 유효성 검사")
+  @Test
+  void ExtensionRequestValidation() {
+    //given
+    ExtensionRequest dto =
+        new ExtensionRequest("");
+
+    //when
+    Set<ConstraintViolation<ExtensionRequest>> violations = validator.validate(dto);
+
+    //then
+    for (ConstraintViolation<ExtensionRequest> violation: violations) {
+      assertThat(violation.getPropertyPath().toString()).isEqualTo("period");
+      assertThat(violation.getMessage()).contains("지정된 항목 중 하나를 입력해야 합니다. 입력 가능 항목(설명): ");
+    }
+    assertThat(violations.size()).isEqualTo(1);
+  }
+
+  @DisplayName("DomainDto.UpdateRequest 유효성 검사")
+  @Test
+  void UpdateRequestValidation() {
+    //given
+    UpdateRequest dto =
+        new UpdateRequest(
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            " ",
+            "  "
+        );
+
+    //when
+    Set<ConstraintViolation<UpdateRequest>> violations = validator.validate(dto);
+
+    //then
+    for (ConstraintViolation<UpdateRequest> violation: violations) {
+      switch (violation.getPropertyPath().toString()) {
+        case "domain":
+          assertThat(violation.getMessage()).isEqualTo("도메인 형식이 잘못되었습니다.");
+          break;
+        case "companyName":
+          assertThat(violation.getMessage()).isEqualTo("회사명은 필수로, 빈 칸으로만 이루어질 수 없습니다.");
+          break;
+        case "businessNumber":
+          assertThat(violation.getMessage()).isEqualTo("사업자 번호 형식이 잘못되었습니다. 사업자 번호는 000-00-00000 형식으로 입력되어야 합니다.");
+          break;
+        case "zipCode":
+          assertThat(violation.getMessage()).isEqualTo("우편번호 입력 형식이 잘못되었습니다. 우편번호는 숫자 5자리로만 입력되어야 합니다.");
+          break;
+        case "roadAddress":
+          assertThat(violation.getMessage()).isEqualTo("도로명 주소 입력은 필수로, 빈 칸으로만 이루어질 수 없습니다.");
+          break;
+        case "address":
+          assertThat(violation.getMessage()).isEqualTo("지번 입력은 필수로, 빈 칸으로만 이루어질 수 없습니다.");
+          break;
+        case "detail":
+          assertThat(violation.getMessage()).isEqualTo("미입력(null), 공백은 입력 가능하나 빈 칸으로만 이루어진 값은 입력할 수 없습니다.");
+          break;
+        case "phoneNumber":
+          assertThat(violation.getMessage()).isEqualTo("전화번호 입력 형식이 잘못되었습니다. 전화번호는 000-0000-0000 형식으로 입력되어야 합니다.");
+          break;
+        default:
+          break;
+      }
+    }
+    assertThat(violations.size()).isEqualTo(2);
+  }
+}
