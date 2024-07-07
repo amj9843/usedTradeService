1481c465ffff30876d636d409ce995a25e666b91
amj9843 <owo9@kakao.com>
2024-07-07 15:29:42 +0900

    feature: Domain 관련 dto 생성

---
+++ src/main/java/com/zerobase/used_trade/data/dto/DomainDto.java
@@ -0,0 +1,177 @@
+package com.zerobase.used_trade.data.dto;
+
+import com.zerobase.used_trade.annotation.BusinessNumber;
+import com.zerobase.used_trade.annotation.DomainPattern;
+import com.zerobase.used_trade.annotation.EmptyOrNotBlank;
+import com.zerobase.used_trade.annotation.PhoneNumber;
+import com.zerobase.used_trade.annotation.ValidEnum;
+import com.zerobase.used_trade.annotation.ZipCode;
+import com.zerobase.used_trade.data.constant.SubscribeType;
+import com.zerobase.used_trade.data.domain.Domain;
+import com.zerobase.used_trade.data.dto.UserDto.Employee;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+public class DomainDto {
+  @Data
+  @Builder
+  public static class Principle {
+    private Long id;
+    private String domainAddress;
+    private String companyName;
+    private String businessNumber;
+    private String zipCode;
+    private String roadAddress;
+    private String address;
+    private String detail;
+    private String phoneNumber;
+    private LocalDateTime endAt;
+
+    public static Principle fromEntity(Domain domain) {
+      return Principle.builder()
+          .id(domain.getId())
+          .domainAddress(domain.getDomainAddress())
+          .companyName(domain.getCompanyName())
+          .businessNumber(domain.getBusinessNumber())
+          .zipCode(domain.getZipCode())
+          .roadAddress(domain.getRoadAddress())
+          .address(domain.getAddress())
+          .detail(domain.getDetail())
+          .phoneNumber(domain.getPhoneNumber())
+          .endAt(domain.getEndAt())
+          .build();
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class EnrollRequest {
+    @NotEmpty(message = "{validation.Domain.address.NotEmpty}")
+    @DomainPattern
+    private String domainAddress;
+
+    @NotBlank(message = "{validation.Company.name.NotBlank}")
+    private String companyName;
+
+    @NotEmpty(message = "{validation.Company.businessNumber.NotEmpty}")
+    @BusinessNumber
+    private String businessNumber;
+
+    @NotEmpty(message = "{validation.Address.zipCode.NotEmpty}")
+    @ZipCode
+    private String zipCode;
+
+    @NotBlank(message = "{validation.Address.roadAddress.NotBlank}")
+    private String roadAddress;
+
+    @NotBlank(message = "{validation.Address.address.NotBlank}")
+    private String address;
+
+    @EmptyOrNotBlank
+    private String detail;
+
+    @NotEmpty(message = "{validation.Company.phoneNumber.NotEmpty}")
+    @PhoneNumber
+    private String phoneNumber;
+
+    @ValidEnum(enumClass = SubscribeType.class)
+    private String period;
+
+    public Domain toEntity() {
+      LocalDateTime endAt =
+          SubscribeType.valueOf(this.period).extension(LocalDateTime.now());
+
+      return Domain.builder()
+          .domainAddress(this.domainAddress)
+          .companyName(this.companyName)
+          .businessNumber(this.businessNumber)
+          .zipCode(this.zipCode)
+          .roadAddress(this.roadAddress)
+          .address(this.address)
+          .detail(this.detail)
+          .phoneNumber(this.phoneNumber)
+          .endAt(endAt)
+          .build();
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class ExtensionRequest {
+    @ValidEnum(enumClass = SubscribeType.class)
+    private String period;
+  }
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class UpdateRequest{
+    @DomainPattern
+    private String domainAddress;
+
+    @EmptyOrNotBlank
+    private String companyName;
+
+    @BusinessNumber
+    private String businessNumber;
+
+    @ZipCode
+    private String zipCode;
+
+    @EmptyOrNotBlank
+    private String roadAddress;
+
+    @EmptyOrNotBlank
+    private String address;
+
+    @EmptyOrNotBlank
+    private String detail;
+
+    @PhoneNumber
+    private String phoneNumber;
+  }
+
+  @Data
+  @NoArgsConstructor
+  public static class DetailInfoResponse{
+    private String domainAddress;
+    private boolean valid;
+    private CompanyInfo companyInformation;
+    private Page<Employee> employees;
+
+    public DetailInfoResponse(Domain domainAddress, Page<Employee> employees) {
+      this.domainAddress = domainAddress.getDomainAddress();
+      this.valid = domainAddress.getEndAt().isAfter(LocalDateTime.now());
+      this.companyInformation = CompanyInfo.builder()
+          .name(domainAddress.getCompanyName())
+          .businessNumber(domainAddress.getBusinessNumber())
+          .zipCode(domainAddress.getZipCode())
+          .roadAddress(domainAddress.getRoadAddress())
+          .address(domainAddress.getAddress())
+          .detail(domainAddress.getDetail())
+          .phoneNumber(domainAddress.getPhoneNumber())
+          .build();
+      this.employees = employees;
+    }
+  }
+
+  @Data
+  @Builder
+  public static class CompanyInfo{
+    private String name;
+    private String businessNumber;
+    private String zipCode;
+    private String roadAddress;
+    private String address;
+    private String detail;
+    private String phoneNumber;
+  }
+}
