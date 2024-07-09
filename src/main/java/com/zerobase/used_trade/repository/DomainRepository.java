7871e3aa27cd46e51b255fb306b4abc21be3d533
amj9843 <owo9@kakao.com>
2024-07-09 17:51:23 +0900

    feat: repository 에 Specification 확장

--- src/main/java/com/zerobase/used_trade/repository/DomainRepository.java
+++ src/main/java/com/zerobase/used_trade/repository/DomainRepository.java
@@ -2,10 +2,11 @@ package com.zerobase.used_trade.repository;
 
 import com.zerobase.used_trade.data.domain.Domain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DomainRepository extends JpaRepository<Domain, Long> {
+public interface DomainRepository extends JpaRepository<Domain, Long>, JpaSpecificationExecutor<Domain> {
   //도메인 주소 중복값 확인
   boolean existsByDomainAddress(String domainAddress);
 }
