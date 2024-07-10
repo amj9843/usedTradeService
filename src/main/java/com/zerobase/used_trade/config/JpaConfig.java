85f76d6c3214337473c8fd77dea64c87520cd305
amj9843 <owo9@kakao.com>
2024-07-10 15:06:16 +0900

    feat: QueryDSL을 사용하기 위한 jpaQueryFactory 등록

--- src/main/java/com/zerobase/used_trade/config/JpaConfig.java
+++ src/main/java/com/zerobase/used_trade/config/JpaConfig.java
@@ -1,10 +1,17 @@
 package com.zerobase.used_trade.config;
 
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-
+  @Bean
+  public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+    return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+  }
 }
