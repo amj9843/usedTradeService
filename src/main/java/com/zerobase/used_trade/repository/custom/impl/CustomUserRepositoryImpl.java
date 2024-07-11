63f191b6361275db7cf365e6edb68ddceed01f8a
amj9843 <owo9@kakao.com>
2024-07-12 02:30:24 +0900

    feat: 계산에 대한 설명 주석 추가, static 변수로 지정

--- src/main/java/com/zerobase/used_trade/repository/custom/impl/CustomUserRepositoryImpl.java
+++ src/main/java/com/zerobase/used_trade/repository/custom/impl/CustomUserRepositoryImpl.java
@@ -1,5 +1,10 @@
 package com.zerobase.used_trade.repository.custom.impl;
 
+import static com.zerobase.used_trade.util.ScoreUtility.BASIC_REVIEW_SCORE;
+import static com.zerobase.used_trade.util.ScoreUtility.BASIC_SCORE;
+import static com.zerobase.used_trade.util.ScoreUtility.HIGHEST_SCORE;
+import static com.zerobase.used_trade.util.ScoreUtility.LOWEST_SCORE;
+
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zerobase.used_trade.data.domain.QDomain;
@@ -26,19 +31,25 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
         .select(
             new QUserDto_Employee(
                 user.id, user.email, user.name, user.nickName, user.phoneNumber,
+                /* 검색한 도메인을 메일 주소로 이용하고 있는 사용자의 '판매 점수' 계산
+                 *
+                 * 최종 점수는 기본점수에 review 에서 평가받은 점수를 토대로 산출,
+                 * 중간점수인 3을 기준으로 각 리뷰에서 받은 점수의 차를 기본 점수에서 계산
+                 * 최소/최대 범위를 넘어갈 경우 값 고정
+                 */
                 new CaseBuilder()
-                    .when(review.dealerScore.sum().add(50).add(
-                        review.dealerScore.count().multiply(-3))
-                        .coalesce(50).gt(100))
-                    .then(100)
-                    .when(review.dealerScore.sum().add(50).add(
-                        review.dealerScore.count().multiply(-3))
-                        .coalesce(50).lt(0))
-                    .then(0)
+                    .when(review.dealerScore.sum().add(BASIC_SCORE).add(
+                        review.dealerScore.count().multiply(-1 * BASIC_REVIEW_SCORE))
+                        .coalesce(BASIC_SCORE).gt(HIGHEST_SCORE))
+                    .then(HIGHEST_SCORE)
+                    .when(review.dealerScore.sum().add(BASIC_SCORE).add(
+                        review.dealerScore.count().multiply(-1 * BASIC_REVIEW_SCORE))
+                        .coalesce(BASIC_SCORE).lt(LOWEST_SCORE))
+                    .then(LOWEST_SCORE)
                     .otherwise(
-                        review.dealerScore.sum().add(50).add(
-                        review.dealerScore.count().multiply(-3))
-                            .coalesce(50)).as("sellScore"))
+                        review.dealerScore.sum().add(BASIC_SCORE).add(
+                        review.dealerScore.count().multiply(-1 * BASIC_REVIEW_SCORE))
+                            .coalesce(BASIC_SCORE)).as("sellScore"))
         ).from(user)
         .leftJoin(domain).on(user.domainId.eq(domain.id))
         .leftJoin(review).on(review.dealerId.eq(user.id))
---
+++ src/main/java/com/zerobase/used_trade/util/ScoreUtility.java
@@ -0,0 +1,10 @@
+package com.zerobase.used_trade.util;
+
+public class ScoreUtility {
+  public final static int BASIC_SCORE = 50;
+  public final static int LOWEST_SCORE = 0;
+  public final static int HIGHEST_SCORE = 100;
+  public final static int LOWEST_REVIEW_SCORE = 1;
+  public final static int HIGHEST_REVIEW_SCORE = 5;
+  public final static int BASIC_REVIEW_SCORE = (LOWEST_REVIEW_SCORE + HIGHEST_REVIEW_SCORE) / 2;
+}
