908cbd7d8a16e7b0a43a8900dba30952538bedd2
amj9843 <owo9@kakao.com>
2024-07-14 21:00:40 +0900

    feat: 성공 코드 추가

--- src/main/java/com/zerobase/used_trade/data/constant/SuccessCode.java
+++ src/main/java/com/zerobase/used_trade/data/constant/SuccessCode.java
@@ -8,7 +8,8 @@ public enum SuccessCode implements CodeAware {
   READ_SUCCESS(HttpStatus.OK, "요청 정보를 성공적으로 가져왔습니다."),
   UPDATED_SUCCESS(HttpStatus.NO_CONTENT, "성공적으로 수정했습니다."),
   EXTEND_PERIOD_SUCCESS(HttpStatus.NO_CONTENT, "만료일을 연장했습니다."),
-  DELETED_SUCCESS(HttpStatus.NO_CONTENT, "요청 정보가 성공적으로 삭제되었습니다.");
+  DELETED_SUCCESS(HttpStatus.NO_CONTENT, "요청 정보가 성공적으로 삭제되었습니다."),
+  SIGN_IN_SUCCESS(HttpStatus.OK, "성공적으로 로그인했습니다.");
 
   private final HttpStatus status;
   private final String message;
