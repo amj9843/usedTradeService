a61373d008f7f861e987cfc0a053126cfa4523c6
amj9843 <owo9@kakao.com>
2024-07-14 20:52:21 +0900

    feat: swagger에 로그인하는 항목 추가

--- src/main/java/com/zerobase/used_trade/config/SwaggerConfig.java
+++ src/main/java/com/zerobase/used_trade/config/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.zerobase.used_trade.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +16,9 @@ public class SwaggerConfig {
   public OpenAPI openAPI() {
     return new OpenAPI()
         .components(new Components())
-        .info(apiInfo());
+        .info(apiInfo())
+        .addSecurityItem(securityRequirement())
+        .components(components());
   }
 
   private Info apiInfo() {
@@ -20,5 +26,22 @@ public class SwaggerConfig {
         .title("중고 거래 서비스")
         .description("중고 상품 거래, 위탁 판매를 도와주는 서비스")
         .version("1.0.0");
+  }
+
+  //인증정보 리스트
+  private SecurityRequirement securityRequirement() {
+    return new SecurityRequirement()
+        .addList("signUserId");
+  }
+
+  private SecurityScheme signUserId() {
+    return new SecurityScheme().type(Type.APIKEY)
+        .in(In.HEADER)
+        .name("Authorization");
+  }
+
+  private Components components() {
+    return new Components()
+        .addSecuritySchemes("signUserId", signUserId());
   }
 }
