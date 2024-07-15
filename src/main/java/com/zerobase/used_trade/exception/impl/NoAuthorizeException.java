7b1c90697462a44a8e9c92c9ce6733eb04b65d8c
amj9843 <owo9@kakao.com>
2024-07-16 00:18:49 +0900

    feat: 사용되는 예외들 모음

---
+++ src/main/java/com/zerobase/used_trade/exception/impl/NoAuthorizeException.java
@@ -0,0 +1,19 @@
+package com.zerobase.used_trade.exception.impl;
+
+import com.zerobase.used_trade.data.constant.ExceptionCode;
+import com.zerobase.used_trade.exception.AbstractException;
+import org.springframework.http.HttpStatus;
+
+public class NoAuthorizeException extends AbstractException {
+  final static ExceptionCode code = ExceptionCode.NO_AUTHORIZE;
+
+  @Override
+  public HttpStatus getStatusCode() {
+    return code.status();
+  }
+
+  @Override
+  public String getMessage() {
+    return code.message();
+  }
+}
