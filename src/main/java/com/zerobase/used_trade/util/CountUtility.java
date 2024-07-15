5bf8408a2d6c2ac8c26ea4eab79a271b43ba066d
amj9843 <owo9@kakao.com>
2024-07-16 00:19:29 +0900

    feat: 변수 지정

---
+++ src/main/java/com/zerobase/used_trade/util/CountUtility.java
@@ -0,0 +1,11 @@
+package com.zerobase.used_trade.util;
+
+public class CountUtility {
+  public final static int USER_MAX_ACCOUNT = 5;
+  public final static int USER_MAX_ADDRESS = 5;
+  public final static int ADMIN_MAX_ADDRESS = 1;
+
+  public final static int PRODUCT_MAX_IMG = 5;
+  public final static int REPORT_MAX_IMG = 5;
+  public final static int REVIEW_MAX_IMG = 5;
+}
