b7fdf7720f187af425df88cb4a3b1564d6e53e91
amj9843 <owo9@kakao.com>
2024-07-16 20:24:47 +0900

    feat: 유니크 설정, update request를 받아 값이 있는지 확인, 수정하는 것으로 수정

--- src/main/java/com/zerobase/used_trade/data/domain/Category.java
+++ src/main/java/com/zerobase/used_trade/data/domain/Category.java
@@ -1,5 +1,6 @@
 package com.zerobase.used_trade.data.domain;
 
+import com.zerobase.used_trade.data.dto.CategoryDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,11 +21,20 @@ public class Category {
   @Column(name = "category_id")
   private Long id;
 
-  @Column(name = "name")
+  @Column(name = "name", unique = true)
   private String name;
 
   @Column(name = "description")
   private String description;
+
+  public void update(CategoryDto.UpdateRequest request) {
+    if (request.getName() != null && !request.getName().isBlank()) {
+      this.name = request.getName().trim();
+    }
+    if (request.getDescription() != null) {
+      this.description = request.getDescription();
+    }
+  }
 
   @Builder
   public Category(String name, String description) {
