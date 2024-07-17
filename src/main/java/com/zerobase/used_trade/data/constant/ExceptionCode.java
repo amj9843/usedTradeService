080df1279f26cc887d84944163ca6545f046b0f4
amj9843 <owo9@kakao.com>
2024-07-17 21:42:42 +0900

    feat: 예외 처리 생성

--- src/main/java/com/zerobase/used_trade/data/constant/ExceptionCode.java
+++ src/main/java/com/zerobase/used_trade/data/constant/ExceptionCode.java
@@ -25,7 +25,15 @@ public enum ExceptionCode implements CodeAware {
   CANNOT_DELETE_ONLY_ONE(HttpStatus.FORBIDDEN, "대표로 설정될 다른 주소/계좌가 등록되어 있어야 합니다."),
   ALREADY_EXISTS_CATEGORY(HttpStatus.CONFLICT, "이미 중복되는 카테고리명이 있습니다."),
   NO_CATEGORY(HttpStatus.NOT_FOUND, "식별번호에 해당하는 카테고리가 없습니다."),
-  CANNOT_DELETE_CONTAIN_PRODUCT(HttpStatus.FORBIDDEN, "카테고리에 속하는 상품이 있으면 카테고리 삭제가 불가합니다.");
+  CANNOT_DELETE_CONTAIN_PRODUCT(HttpStatus.FORBIDDEN, "카테고리에 속하는 상품이 있으면 카테고리 삭제가 불가합니다."),
+  FAILED_DELETE_IMAGE(HttpStatus.CONFLICT, "이미지 삭제에 실패했습니다."),
+  FAILED_UPLOAD_OBJECT(HttpStatus.CONFLICT, "파일 업로드에 실패했습니다."),
+  INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "업로드하려는 파일 형식이 잘못되었습니다."),
+  CANNOT_UPLOAD_IMAGE_OVER_THE_MAX_COUNT(HttpStatus.BAD_REQUEST, "업로드하려는 이미지의 최대 개수를 넘어섰습니다."),
+  CANNOT_USE_BEFORE_ENROLL_ACCOUNT(HttpStatus.CONFLICT, "계좌 정보를 등록하지 않고 이용할 수 없습니다."),
+  CANNOT_USE_BEFORE_ENROLL_ADDRESS(HttpStatus.CONFLICT, "주소 정보를 등록하지 않고 이용할 수 없습니다."),
+  NO_MATCH_ACCOUNT_AND_USER(HttpStatus.CONFLICT, "사용자가 등록한 계좌 정보 중 입력한 계좌 식별번호와 일치하는 것이 없습니다."),
+  NO_MATCH_ADDRESS_AND_USER(HttpStatus.CONFLICT, "사용자가 등록한 주소 정보 중 입력한 주소 식별번호와 일치하는 것이 없습니다.");
 
   private final HttpStatus status;
   private final String message;
