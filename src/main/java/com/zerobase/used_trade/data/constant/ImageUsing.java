b3230f19413a57b729fc2fde88eafdba89825e79
amj9843 <owo9@kakao.com>
2024-07-19 06:13:51 +0900

    feat: 이미지 관리 기능에 사용되는 DTO, constant 생성 및 수정

--- src/main/java/com/zerobase/used_trade/data/constant/ImageUsing.java
+++ src/main/java/com/zerobase/used_trade/data/constant/ImageUsing.java
@@ -1,18 +1,36 @@
 package com.zerobase.used_trade.data.constant;
 
+import static com.zerobase.used_trade.util.CountUtility.PRODUCT_MAX_IMG;
+import static com.zerobase.used_trade.util.CountUtility.REPORT_MAX_IMG;
+import static com.zerobase.used_trade.util.CountUtility.REVIEW_MAX_IMG;
+import static com.zerobase.used_trade.util.FileUtility.PRODUCT_FOLDER;
+import static com.zerobase.used_trade.util.FileUtility.REPORT_FOLDER;
+import static com.zerobase.used_trade.util.FileUtility.REVIEW_FOLDER;
 import static java.lang.String.format;
 
 import com.zerobase.used_trade.data.constant.aware.DescriptionAware;
 
 public enum ImageUsing implements DescriptionAware {
-  PRODUCT("상품 이미지"),
-  REPORT("신고/건의 이미지"),
-  REVIEW("후기 이미지");
+  PRODUCT("상품 이미지", PRODUCT_FOLDER, PRODUCT_MAX_IMG),
+  REPORT("신고/건의 이미지", REPORT_FOLDER, REPORT_MAX_IMG),
+  REVIEW("후기 이미지", REVIEW_FOLDER, REVIEW_MAX_IMG);
 
   private final String description;
+  private final String folder;
+  private final int maxCount;
 
-  ImageUsing(String description) {
+  ImageUsing(String description, String folder, int maxCount) {
     this.description = description;
+    this.folder = folder;
+    this.maxCount = maxCount;
+  }
+
+  public String folder() {
+    return this.folder;
+  }
+
+  public int maxCount() {
+    return this.maxCount;
   }
 
   @Override
