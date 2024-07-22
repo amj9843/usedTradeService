de21f45fe8857bfa6cab64f209a9a2a5df6fd7a7
amj9843 <owo9@kakao.com>
2024-07-22 20:27:22 +0900

    feat: 사용되는 쿼리 함수 생성

--- src/main/java/com/zerobase/used_trade/repository/custom/CustomDealRepository.java
+++ src/main/java/com/zerobase/used_trade/repository/custom/CustomDealRepository.java
@@ -1,5 +1,18 @@
 package com.zerobase.used_trade.repository.custom;
 
+import com.zerobase.used_trade.data.constant.DealMethodFilterType;
+import com.zerobase.used_trade.data.constant.DealStatusFilterType;
+import com.zerobase.used_trade.data.domain.Deal;
+import com.zerobase.used_trade.data.dto.DealDto.SimpleInfoResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CustomDealRepository {
   boolean isApplyingOrProcessingByDealMethodId(Long dealMethodId);
+
+  List<SimpleInfoResponse> findAllOfSimple(
+      Long productId, Long buyerId, LocalDateTime applyTimeAfter, LocalDateTime applyTimeBefore,
+      DealMethodFilterType methodFilter, DealStatusFilterType statusFilter);
+  boolean isSeller(Long userId, Long dealId);
+  List<Deal> findAllByProductIdExceptDealId(Long productId, Long dealId);
 }
--- src/main/java/com/zerobase/used_trade/repository/custom/impl/CustomDealMethodRepositoryImpl.java
+++ src/main/java/com/zerobase/used_trade/repository/custom/impl/CustomDealMethodRepositoryImpl.java
@@ -3,11 +3,13 @@ package com.zerobase.used_trade.repository.custom.impl;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zerobase.used_trade.data.constant.ProductStatus;
 import com.zerobase.used_trade.data.domain.DealMethod;
+import com.zerobase.used_trade.data.domain.QDeal;
 import com.zerobase.used_trade.data.domain.QDealMethod;
 import com.zerobase.used_trade.data.domain.QProduct;
 import com.zerobase.used_trade.repository.custom.CustomDealMethodRepository;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +20,7 @@ public class CustomDealMethodRepositoryImpl implements CustomDealMethodRepositor
 
   private final QDealMethod dealMethod = QDealMethod.dealMethod;
   private final QProduct product = QProduct.product;
+  private final QDeal deal = QDeal.deal;
 
   @Override
   public List<DealMethod> findAllByProductIdPossible(Long productId) {
@@ -34,5 +37,16 @@ public class CustomDealMethodRepositoryImpl implements CustomDealMethodRepositor
             )
         ).orderBy(dealMethod.dateTime.asc().nullsFirst())
         .fetch();
+  }
+
+  @Override
+  public Optional<DealMethod> findByDealId(Long dealId) {
+    return Optional.ofNullable(jpaQueryFactory
+        .select(dealMethod)
+        .from(dealMethod)
+        .leftJoin(deal).on(deal.detailId.eq(dealMethod.id))
+        .where(
+            deal.id.eq(dealId)
+        ).fetchFirst());
   }
 }
